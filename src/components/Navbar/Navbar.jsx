fac8ec121f755170ac464af35e5086b5769acbc3
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-08-31 22:03:59 +0600

    contact functionality added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ShinyText from "../ShinyText/ShinyText";
 
-const Navbar = () => {
+const Navbar = ({contactRef}) => {
+
+  const handleContactClick = () =>{
+    if(contactRef.current){
+      contactRef.current.scrollIntoView({behavior: "smooth"})
+    }
+  }
+
   return (
     <div className="bg-[#1d1c2291] text-center max-w-[1240px] mx-auto my-[20px] md:my-[50px] p-[32px] flex md:flex-row items-center justify-between rounded-lg">
       <div>
@@ -28,7 +35,7 @@ const Navbar = () => {
           <h1>Testimonials</h1>
         </div>
       </div>
-      <div className="bg-[#EBF3FA] mt-2 md:mt-0 px-[24px] py-3 text-center rounded-lg">
+      <div onClick={() => handleContactClick()} className="bg-[#EBF3FA] mt-2 md:mt-0 px-[24px] py-3 text-center rounded-lg">
         <ShinyText
           text="Contact"
           disabled={false}
